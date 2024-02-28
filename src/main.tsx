8d42df63bd402ebd20ccb55195acb107354cda8b
borodinka <sofibor03@gmail.com>
2024-02-27 22:52:19 -0500

    Merged main

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import { store } from "@store/store.ts";
+
+import App from "./app/App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
---
+++ src/pages/login.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+
+import { APP_NAME } from "@config/constants";
+import LoginForm from "@features/auth/components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Typography component="h1" variant="h2" mb={1}>
+        Login
+      </Typography>
+      <Typography color="text.secondary" mb={4}>
+        Login to access {APP_NAME}
+      </Typography>
+      <LoginForm />
+    </>
+  );
+}
